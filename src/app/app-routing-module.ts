4549c568c14f046861c355f77960ab48ef2d191c
LisethBuitrago <lisethramireeez@gmail.com>
2026-05-11 23:07:59 -0500

    Inicio sesion Usuario

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -3,6 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { Idioma } from './idioma/idioma';
 import { Inicio } from './inicio/inicio';
 import { LoginAdministrador } from './login-administrador/login-administrador';
+import {LoginUsuario} from './login-usuario/login-usuario';
+import {CrearcuentaUsuario} from './crearcuenta-usuario/crearcuenta-usuario';
 
 const routes: Routes = [
 
@@ -10,7 +12,9 @@ const routes: Routes = [
   { path: 'inicio', component: Inicio },
   { path: 'idioma', component: Idioma },
   {path: 'login-administrador', component: LoginAdministrador },
-  { path: '**', redirectTo: '' }
+  {path: 'login-usuario', component: LoginUsuario},
+  {path: 'crearcuenta-usuario', component: CrearcuentaUsuario},
+  { path: '**', redirectTo: '' },  //esta linea siempre debe ir a lo ultimo att:lis
 ];
 
 @NgModule({
