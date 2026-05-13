edc36b130aa3036c8d0e2bb0a3eef41b859dd796
sanchezgina0312 <sanchez.gina0312@gmail.com>
2026-05-12 21:09:26 -0500

    Deleted

--- src/app/crearcuenta-usuario/crearcuenta-usuario.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-crearcuenta-usuario',
-  standalone: false,
-  templateUrl: './crearcuenta-usuario.html',
-  styleUrl: './crearcuenta-usuario.css',
-})
-export class CrearcuentaUsuario {}
--- src/app/login-administrador/login-administrador.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-login-administrador',
-  standalone: false,
-  templateUrl: './login-administrador.html',
-  styleUrl: './login-administrador.css',
-})
-export class LoginAdministrador {}
